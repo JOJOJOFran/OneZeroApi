44512b9b812a927dd52cb904fc3d68f9fe7dc96b
fran <1317932489@qq.com>
2019-02-13 00:10:36 +0800

    00

---
+++ src/OneZero.Common/Extensions/SecretHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace OneZero.Common.Extensions
+{
+    public static class SecretHelper
+    {
+        /// <summary>
+        /// 密码MD5加密
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string MD5Hash(this string input)
+        {
+            using (var md5 = MD5.Create())
+            {
+                try
+                {
+                    var strResult = md5.ComputeHash(Encoding.ASCII.GetBytes(input));
+                    return BitConverter.ToString(strResult).Replace("-", "");
+                }
+                catch (Exception e)
+                {
+                    return "";
+                }
+
+            }
+        }
+    }
+}
